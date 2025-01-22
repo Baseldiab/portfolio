2e57b45d859ac064dc81e8591afb4eb7ec671d3e
Basel Diab <114107867+Baseldiab@users.noreply.github.com>
2025-01-23 01:17:53 +0200

    add links and update footer

--- app/components/footer/footer.tsx
+++ app/components/footer/footer.tsx
@@ -9,9 +9,47 @@ import { useTranslations } from "@/app/components/hooks/useTranslation";
 // components
 import Logo from "@/app/components/common/logo";
 
+// Types
+import { ContactLink } from "@/app/components/interfaces/contact-links";
+
+// icons
+import { Linkedin, Mail, Phone } from "lucide-react";
+import GithubIcon from "@/app/components/icons/githubIcon";
+import WhatsAppIcon from "@/app/components/icons/whatsAppIcon";
+
 export default function Footer({ params: { locale } }: LocalProps) {
   const { t } = useTranslations(locale as string);
   const yearNow = new Date().getFullYear();
+
+  const iconClassName = "size-4 lg:size-5 group-hover:text-white ";
+
+  const contactLinks: ContactLink[] = [
+    {
+      id: "contact-linkedIn",
+      href: "https://www.linkedin.com/in/basel-diab-94b526259/",
+      icon: <Linkedin className={iconClassName} />,
+    },
+    {
+      id: "contact-github",
+      href: "https://github.com/Baseldiab",
+      icon: <GithubIcon className={iconClassName} />,
+    },
+    {
+      id: "contact-mail",
+      href: "mailto:baseldiab21@gmail.com",
+      icon: <Mail className={iconClassName} />,
+    },
+    {
+      id: "contact-phone",
+      href: "tel:+201068769643",
+      icon: <Phone className={iconClassName} />,
+    },
+    {
+      id: "contact-whatsapp",
+      href: "https://wa.me/201068769643",
+      icon: <WhatsAppIcon className={iconClassName} />,
+    },
+  ];
 
   return (
     <footer className={`bg-slate-200 shadow-xl dark:bg-white/5  z-30`}>
@@ -29,7 +67,21 @@ export default function Footer({ params: { locale } }: LocalProps) {
             {t("navigation.footer-name")}
           </a>
         </div>
+
+        <ul className="flex justify-center items-center gap-2">
+          {contactLinks.map((item: ContactLink) => (
+            <ContactItem key={item.id} item={item} />
+          ))}
+        </ul>
       </div>
     </footer>
   );
 }
+
+const ContactItem = ({ item }: { item: ContactLink }) => {
+  return (
+    <li className="flex justify-center items-center bg-white dark:bg-black/50 p-2 rounded-md group btn-radial-out">
+      {item.icon}
+    </li>
+  );
+};
---
+++ app/components/interfaces/contact-links.ts
@@ -0,0 +1,7 @@
+import { ReactNode } from "react";
+
+export interface ContactLink {
+    id: string;
+    href: string;
+    icon: ReactNode;
+}
