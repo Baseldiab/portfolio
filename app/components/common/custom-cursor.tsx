7af2d892cc85b3534d5f3260f5307d8ee0e6d991
Basel Diab <114107867+Baseldiab@users.noreply.github.com>
2025-01-27 21:25:41 +0200

    hide custom cursor on md screen

--- app/components/common/custom-cursor.tsx
+++ app/components/common/custom-cursor.tsx
@@ -13,42 +13,44 @@ export default function CustomCursor() {
   if (!mounted) return null;
 
   return (
-    <AnimatedCursor
-      innerSize={8}
-      outerSize={8}
-      innerScale={1}
-      outerScale={3}
-      outerAlpha={0.3}
-      showSystemCursor={true}
-      innerStyle={{
-        backgroundColor: "transparent",
-        cursor: "default",
-      }}
-      outerStyle={{
-        backgroundColor: "#ffffff",
-        border: "5px solid #ffffff",
-        mixBlendMode: "difference",
-      }}
-      clickables={[
-        ".contact_marquee_link",
-        "a",
-        'input[type="text"]',
-        'input[type="email"]',
-        'input[type="number"]',
-        "input[type='submit']",
-        "input[type='image']",
-        "label[for]",
-        "select",
-        "textarea",
-        "button",
-        ".link",
-        {
-          target: ".custom",
-          outerSize: 20,
-          outerAlpha: 0.3,
-          outerScale: 5,
-        },
-      ]}
-    />
+    <div className="hidden md:block">
+      <AnimatedCursor
+        innerSize={8}
+        outerSize={8}
+        innerScale={1}
+        outerScale={3}
+        outerAlpha={0.3}
+        showSystemCursor={true}
+        innerStyle={{
+          backgroundColor: "transparent",
+          cursor: "default",
+        }}
+        outerStyle={{
+          backgroundColor: "#ffffff",
+          border: "5px solid #ffffff",
+          mixBlendMode: "difference",
+        }}
+        clickables={[
+          ".contact_marquee_link",
+          "a",
+          'input[type="text"]',
+          'input[type="email"]',
+          'input[type="number"]',
+          "input[type='submit']",
+          "input[type='image']",
+          "label[for]",
+          "select",
+          "textarea",
+          "button",
+          ".link",
+          {
+            target: ".custom",
+            outerSize: 20,
+            outerAlpha: 0.3,
+            outerScale: 5,
+          },
+        ]}
+      />
+    </div>
   );
 }
