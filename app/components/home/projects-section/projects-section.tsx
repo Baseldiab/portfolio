b1928f474a008e6688e23d34e59cb74b71d1e12b
Basel Diab <114107867+Baseldiab@users.noreply.github.com>
2025-01-27 23:05:09 +0200

    upload portfolio

--- app/components/home/projects-section/projects-section.tsx
+++ app/components/home/projects-section/projects-section.tsx
@@ -3,18 +3,26 @@ import React from "react";
 // lib
 import { cn } from "@/lib/utils";
 
+// i18n
+import getTranslations from "@/app/i18n";
+
 // interfaces
 import { LocalProps } from "@/app/components/interfaces/local.props.interface";
+
+// components common
+import SectionTitle from "@/app/components/common/section-title";
 
 // components projects
 import ShownCards from "@/app/components/home/projects-section/components/shown-cards";
 
-export default function ProjectsSection({
+export default async function ProjectsSection({
   params: { locale },
   className,
 }: LocalProps & {
   className?: string;
 }) {
+  const { t } = await getTranslations(locale as string);
+
   return (
     <section
       id="projects"
@@ -23,6 +31,8 @@ export default function ProjectsSection({
         width: "calc(100vw - 10px)",
       }}
     >
+      <SectionTitle title={t("fields.projects.title")} />
+
       <ShownCards />
     </section>
   );
